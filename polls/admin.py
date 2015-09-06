6c1f7bb407affcbe0e3c214bfa3ea0398a6c9171
glloyd11 <lloydg3@verizon.net>
2015-09-06 11:39:29 -0400

    Added Blog Object

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,7 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from .models import Post, Question, Choice
+
+admin.site.register(Post)
+admin.site.register(Question)
+admin.site.register(Choice)
--- polls/models.py
+++ polls/models.py
@@ -18,3 +18,19 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):              # __unicode__ on Python 2
         return self.choice_text
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
